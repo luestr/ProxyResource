fa0f6bb91f7329358707bfc19a4362f29170ca2f
ketabo <ketabo6290@kravify.com>
2024-06-02 22:21:22 +0800

    同步仓库

--- Resource/Script/WexinMiniPrograms/chayanyuese/chayanyuese_remove_ads.js
+++ Resource/Script/WexinMiniPrograms/chayanyuese/chayanyuese_remove_ads.js
@@ -4,6 +4,6 @@ delete obj.data.INDEX_SLOT_02;
 obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
   delete item.bubble; 
   delete item.figure; 
-  item.value = "https://gitlab.com/lodepuly/vpn_tool/-/raw/master/Resource/Script/WexinMiniPrograms/chayanyuese/cyys1.jpg";
+  item.value = "https://raw.githubusercontent.com/luestr/ProxyResource/main/Resource/Script/WexinMiniPrograms/chayanyuese/cyys1.jpg";
 });
 $done({body: JSON.stringify(obj)});
